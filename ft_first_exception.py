9a464018915186b1630dec25904c0cd3f41861bb
PaulPouss <paul.poussereau@gmail.com>
2026-04-06 10:09:34 +0200

    Add files via upload

--- ft_first_exception.py
+++ ft_first_exception.py
@@ -1,10 +1,22 @@
 def input_temperature(temp_str: str) -> int:
     print(f"Input data is '{temp_str}'")
-    return (int(temp_str))
+    try:
+        result = int(temp_str)
+        return result
+    except ValueError:
+        print("Value error")
+        return False
 
 
-def main():
-    print(f"{input_temperature("abc")}")
+def test_temperature() -> None:
+    input_temperature("abc")
+    input_temperature("25")
+    input_temperature("50")
+    input_temperature("100")
+
+
+def main() -> None:
+    test_temperature()
 
 
 if __name__ == "__main__":
