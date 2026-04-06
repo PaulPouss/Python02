0f8670e349905e5c3cd9bd644e3010f07ab7caf6
PaulPouss <paul.poussereau@gmail.com>
2026-04-06 10:23:14 +0200

    Add files via upload

--- ft_different_errors.py
+++ ft_different_errors.py
@@ -8,3 +8,30 @@ def garden_operations(operation_number: int) -> None:
     elif (operation_number == 3):
         raise (TypeError)
     return
+
+
+def test_error_types() -> None:
+    for value in range(5):
+        print(f"Testing operation {value}...")
+        try:
+            garden_operations(value)
+            print("Operation completed successfully")
+        except ValueError:
+            print("Caught ValueError: ", end="")
+            print("invalid literal for int() with base 10: 'abc'")
+        except ZeroDivisionError:
+            print("Caught ZeroDivisionError: division by zero")
+        except FileNotFoundError:
+            print("Caught FileNotfoundError: ", end="")
+            print("[Errno 2] No such file or directory: 'non/existent/file'")
+        except TypeError:
+            print("Caught TypeError: ", end="")
+            print("can only concatenate str (not ""int"") to str")
+
+
+def main() -> None:
+    test_error_types()
+
+
+if __name__ == "__main__":
+    main()
